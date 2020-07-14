891a274732873207f2f418c996fd5c5faae9d2fe
Leonardo souza <leonardotreze@gmail.com>
2020-07-13 21:38:45 -0300

    react-native clean

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  View,
-} from 'react-native';
 import Home from './src/home';
 import Header from './src/header';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.appContainer}>
-        <Header title="Random People" />
+      <div>
+        <Header title="myfin-react" />
         <Home />
-      </View>
+      </div>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-  },
-});
-
-AppRegistry.registerComponent("App", () => App);
